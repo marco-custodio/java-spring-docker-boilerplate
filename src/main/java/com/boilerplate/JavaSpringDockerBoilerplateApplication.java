84bb01f2c896f40f2135d76276f7f69d8846a2b3
Marco Custodio <marcoantoniocp.71@gmail.com>
2022-12-14 15:48:31 -0300

    First commit

---
+++ src/main/java/com/boilerplate/JavaSpringDockerBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDockerBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDockerBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boilerplate/JavaSpringDockerBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDockerBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
